1ad67845bf4a50c209a46ba2d6f0d350f0df0e2c
Julien Taillan <julien.taillan@gmail.com>
2022-07-25 23:41:55 +0200

    Correction

--- js/cart.js
+++ js/cart.js
@@ -12,22 +12,18 @@ fetch(`http://localhost:3000/api/products/${id}`)
     })
 
     .then(function (value){
-
-        let productUrl = value.imageUrl;
-        let productAltTxt = value.altTxt;
-
         for (let i=0; i<basketArray.length; i++){
             let mainContainer= document.querySelector('#cart__items');
             mainContainer.innerHTML +=
             `<article class="cart__item" data-id="${basketArray[i].id}" data-color="${basketArray[i].color}">
                 <div class="cart__item__img">
-                    <img src="${productUrl}" alt="${productAltTxt}">
+                    <img src="${basketArray[i].imageUrl}" alt="${basketArray[i].altTxt}">
                 </div>
                 <div class="cart__item__content">
                     <div class="cart__item__content__description">
                         <h2>${basketArray[i].id}</h2>
                         <p>${basketArray[i].color}</p>
-                        <p>${value[i].price}€</p>
+                        <p>${basketArray[i].price}€</p>
                     </div>
                     <div class="cart__item__content__settings">
                         <div class="cart__item__content__settings__quantity">
